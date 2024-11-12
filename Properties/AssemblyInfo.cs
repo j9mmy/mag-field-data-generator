e77d2c2d65254a88d33ee2305791225423084178
j9mmy <jimmy.tukiran@outlook.com>
2024-11-12 23:19:03 +0100

    chore: file size

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Magnetic Field Data Generator")]
+[assembly: AssemblyDescription("A tool to generate synthetic magnetic field data (in mT) for testing and simulation purposes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Magnetic Field Data Generator")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d13a3b3b-3b3b-3b3b-3b3b-3b3b3b3b3b3b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
